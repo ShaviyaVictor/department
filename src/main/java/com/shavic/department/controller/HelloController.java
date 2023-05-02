9d59cf3b55aea477762a145bd2435ed7c83fe67e
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-01 21:21:08 -0500

    Create a simple helloWorld Java method and add the @RequestMapping annotation with 2 params

--- src/main/java/com/shavic/department/controller/HelloController.java
+++ src/main/java/com/shavic/department/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.shavic.department.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //  @Component --> is a stereotype annotation that defines SpringBoot classes as Components so that whenever the app runs the classes are added to the Spring container
@@ -7,7 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 //  @Controller --> a stereotype annotation that clearly defines a class as both a Controller hence has the @Component annotation in it and also defines a class as a Controller
 //  @RestController --> defines a class as a Component since it contains the @Controller annotation within it which has @Component within it thus in turn also defining the class as a Controller;
 //                      and also has the @ResponseBody annotation within it, an annotation that allows the class to return Response Body objects in either JSOn or XML formats
+//  @RequestMapping --> maps requests from the client to the method annotated and can have params, such as,
+//                      value - that assigns the endpoint url path and,
+//                      method - defines the CRUD operation request from the client
 
 @RestController
 public class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello World Java!";
+    }
+
 }
