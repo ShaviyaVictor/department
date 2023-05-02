eda33d954b45ded82e9ac73619150cff8ab3f6c6
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-01 20:51:21 -0500

    Add notes for the main class regarding the annotations for the main class

--- src/main/java/com/shavic/department/DepartmentApplication.java
+++ src/main/java/com/shavic/department/DepartmentApplication.java
@@ -3,6 +3,11 @@ package com.shavic.department;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//	@SpringBootApplication --> is then main annotation in a SpringBoot application
+//		- the annotation contains the below bundled up annotations:
+//			@SpringBootConfiguration --> which tells the application that this is the main SpringBoot application
+//			@EnableAutoConfiguration --> which adds all the auto configurations of the applications but gives an allowance to exclude classes and annotations that you don't want to include automatically
+//			@ComponentScan --> which scans all the components in our application and adds the scanned components to the created SpringBoot container when the app runs
 
 @SpringBootApplication
 public class DepartmentApplication {
