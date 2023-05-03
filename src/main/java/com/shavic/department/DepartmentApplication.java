879274b60468e06f1045e1b5cbd7112f573f46d0
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-02 20:59:34 -0500

    Add the devtools dependency and add comments for the dependencies

--- src/main/java/com/shavic/department/DepartmentApplication.java
+++ src/main/java/com/shavic/department/DepartmentApplication.java
@@ -3,7 +3,7 @@ package com.shavic.department;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//	@SpringBootApplication --> is then main annotation in a SpringBoot application
+//	@SpringBootApplication --> is the main annotation in a SpringBoot application
 //		- the annotation contains the below bundled up annotations:
 //			@SpringBootConfiguration --> which tells the application that this is the main SpringBoot application
 //			@EnableAutoConfiguration --> which adds all the auto configurations of the applications but gives an allowance to exclude classes and annotations that you don't want to include automatically
