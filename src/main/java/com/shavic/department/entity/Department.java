9e32cebf5403fb6acfeca47cea84c7763f75d1f9
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-05 17:01:56 -0500

    Add the properties of the Department Entity

--- src/main/java/com/shavic/department/entity/Department.java
+++ src/main/java/com/shavic/department/entity/Department.java
@@ -2,6 +2,9 @@ package com.shavic.department.entity;
 
 public class Department {
 
-
+    private Long departmentId;
+    private String departmentName;
+    private String departmentHead;
+    private String departmentRole;
 
 }
