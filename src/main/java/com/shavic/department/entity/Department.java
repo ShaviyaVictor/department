07b9d2d6f4e29b21ef5fe4464e51b7d8ee39af53
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-05 18:56:38 -0500

    Add a shortcut used to create the Entitys Getters and Setters

--- src/main/java/com/shavic/department/entity/Department.java
+++ src/main/java/com/shavic/department/entity/Department.java
@@ -7,4 +7,35 @@ public class Department {
     private String departmentHead;
     private String departmentRole;
 
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentHead() {
+        return departmentHead;
+    }
+
+    public void setDepartmentHead(String departmentHead) {
+        this.departmentHead = departmentHead;
+    }
+
+    public String getDepartmentRole() {
+        return departmentRole;
+    }
+
+    public void setDepartmentRole(String departmentRole) {
+        this.departmentRole = departmentRole;
+    }
 }
