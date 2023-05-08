1a97c1f27951040862f5df7e5175c00f571532fd
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-07 19:11:57 -0500

    Extend JpaRepository that extents our Entity to give us built-in methods

--- src/main/java/com/shavic/department/repository/DepartmentRepository.java
+++ src/main/java/com/shavic/department/repository/DepartmentRepository.java
@@ -1,7 +1,9 @@
 package com.shavic.department.repository;
 
+import com.shavic.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
