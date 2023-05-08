06c771aaac6ba38638ec43db933c523c9d97e523
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-08 18:36:54 -0500

    Create the Service Object using 2 ways of doing it

--- src/main/java/com/shavic/department/controller/DepartmentController.java
+++ src/main/java/com/shavic/department/controller/DepartmentController.java
@@ -1,6 +1,9 @@
 package com.shavic.department.controller;
 
 import com.shavic.department.entity.Department;
+import com.shavic.department.service.DepartmentService;
+import com.shavic.department.service.impl.DepartmentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DepartmentController {
 
+    @Autowired
+    private DepartmentService departmentService;
+
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department) {
-
+//        1st way of creating the service object
+//        DepartmentService departmentService = new DepartmentServiceImpl();
     }
 
 }
