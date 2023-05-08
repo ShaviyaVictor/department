a69de8c834d4cd12c2e2138a29f9bfe3d26242bb
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-07 20:50:05 -0500

    Call the RequestBody annotation to convert the JSON to the Object called

--- src/main/java/com/shavic/department/controller/DepartmentController.java
+++ src/main/java/com/shavic/department/controller/DepartmentController.java
@@ -2,13 +2,14 @@ package com.shavic.department.controller;
 
 import com.shavic.department.entity.Department;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
 
     @PostMapping("/departments")
-    public Department saveDepartment() {
+    public Department saveDepartment(@RequestBody Department department) {
 
     }
 
