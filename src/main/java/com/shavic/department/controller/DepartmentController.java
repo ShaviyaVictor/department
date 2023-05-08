b6791d80f9d9e757128b1f3d03e9ae2bf06b439f
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-07 20:39:24 -0500

    Create the save method skeleton

--- src/main/java/com/shavic/department/controller/DepartmentController.java
+++ src/main/java/com/shavic/department/controller/DepartmentController.java
@@ -1,7 +1,15 @@
 package com.shavic.department.controller;
 
+import com.shavic.department.entity.Department;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
+
+    @PostMapping("/departments")
+    public Department saveDepartment() {
+
+    }
+
 }
