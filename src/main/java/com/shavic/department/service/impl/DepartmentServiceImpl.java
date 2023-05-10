2e83f0566aa8b5b09e2fcc39e1e5709f8ced7af9
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-09 20:19:38 -0500

    Create the method implememntation and inject the repository and call the JPA save method to save the JSON object to the Entity

--- src/main/java/com/shavic/department/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/shavic/department/service/impl/DepartmentServiceImpl.java
@@ -1,8 +1,19 @@
 package com.shavic.department.service.impl;
 
+import com.shavic.department.entity.Department;
+import com.shavic.department.repository.DepartmentRepository;
 import com.shavic.department.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
 }
