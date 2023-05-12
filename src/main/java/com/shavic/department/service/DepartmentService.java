5446912a15e1e1d152431d191ae4a23218e6a563
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-11 21:07:25 -0500

    Create the fetchDepart method in the service interface

--- src/main/java/com/shavic/department/service/DepartmentService.java
+++ src/main/java/com/shavic/department/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.shavic.department.service;
 
 import com.shavic.department.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    List<Department> fetchDepartmentList();
 }
