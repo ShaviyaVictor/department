d2af725d592c3eb161eb701988c9122c89fa23b8
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-11 21:11:25 -0500

    Remove the public call form the interface, it is redandant

--- src/main/java/com/shavic/department/service/DepartmentService.java
+++ src/main/java/com/shavic/department/service/DepartmentService.java
@@ -5,7 +5,7 @@ import com.shavic.department.entity.Department;
 import java.util.List;
 
 public interface DepartmentService {
-    public Department saveDepartment(Department department);
+    Department saveDepartment(Department department);
 
     List<Department> fetchDepartmentList();
 }
