58a0792d9acf31fae174cf38e4b611641e5bffaf
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-12 20:49:27 -0500

    Add the URL GetMapping that will take in the Id definition variable as an input to link it to the PathVariable defined in the method input

--- src/main/java/com/shavic/department/controller/DepartmentController.java
+++ src/main/java/com/shavic/department/controller/DepartmentController.java
@@ -3,10 +3,7 @@ package com.shavic.department.controller;
 import com.shavic.department.entity.Department;
 import com.shavic.department.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,8 +27,9 @@ public class DepartmentController {
         return departmentService.fetchDepartmentList();
     }
 
-    public Department fetchDepartmentById() {
-        return null;
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+        return departmentService.fetchDepartmentById(departmentId);
     }
 
 }
