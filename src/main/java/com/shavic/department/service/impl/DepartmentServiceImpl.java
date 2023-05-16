97defa64c3ae4198d9d00f8d1363e87aa119411e
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-15 20:33:48 -0500

    Add logic to check if the Entity properties are NotNull and also if they are not blank and the value is unaltered

--- src/main/java/com/shavic/department/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/shavic/department/service/impl/DepartmentServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
@@ -31,6 +32,30 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Override
     public void deleteDepartmentById(Long departmentId) {
         departmentRepository.deleteById(departmentId);
+    }
+
+    @Override
+    public Department updateDepartment(Long departmentId, Department department) {
+//        get the object Entity of the specified Id from the DB
+        Department departmentObject = departmentRepository.findById(departmentId).get();
+//        logic to check the Entity properties that are null in the incoming changes and if null, skip;
+//        and if an update is present, then update the specified object
+
+//        if departmentName is NOT NULL and is also not blank equals to the available departmentName ignoring the casing;
+//        then skip the departmentName or in other words let the same available value remain.
+//        Same logic check for the other Entity properties: departmentHead and departmentRole
+        if (Objects.nonNull(department.getDepartmentName()) && !"".equalsIgnoreCase(department.getDepartmentName())) {
+            departmentObject.setDepartmentName(department.getDepartmentName());
+        }
+        if (Objects.nonNull(department.getDepartmentHead()) && !"".equalsIgnoreCase(department.getDepartmentHead())) {
+            departmentObject.setDepartmentHead(department.getDepartmentHead());
+        }
+        if (Objects.nonNull(department.getDepartmentRole()) && !"".equalsIgnoreCase(department.getDepartmentRole())) {
+            departmentObject.setDepartmentRole(department.getDepartmentRole());
+        }
+
+        return null;
+
     }
 
 }
