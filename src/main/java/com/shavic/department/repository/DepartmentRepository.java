0cd9fb87c33218d3f29a96641d6df09ac52447a0
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-16 20:16:01 -0500

    Create a custom fetchByDepartmentName in the Reposi

--- src/main/java/com/shavic/department/repository/DepartmentRepository.java
+++ src/main/java/com/shavic/department/repository/DepartmentRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+//JpaRepository takes in the Entity Object and the Primary Key Type
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+//    Modifier 'public' is redundant for interface members hence no initializing the methods in Interfaces with public declaration
+    Department fetchByDepartmentName(String departmentName);
+
 }
