6582ed8a0350c9d67589a03769d9fdfdfa2a693f
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-17 17:41:20 -0500

    Update the documentation of the method implementation

--- src/main/java/com/shavic/department/controller/HelloWorld.java
+++ src/main/java/com/shavic/department/controller/HelloWorld.java
@@ -1,48 +1,56 @@
 package com.shavic.department.controller;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 //    Controller
 //  @Component --> is a generic stereotype annotation that defines SpringBoot classes as Components so that whenever the app runs the classes are added to the Spring container
-//  @Controller, @Service and @Repository annotations are specializations of @Component annotation.
-//  Check here:--> https://stackoverflow.com/questions/6827752/whats-the-difference-between-component-repository-service-annotations-in
-//  Controllers however are not just any class, they are an app resource given they are the Presentational layer of the app container having the app's CRUD operations defined here in
-//  @Controller --> a stereotype annotation that clearly defines a class as both a Controller given it has the @Component annotation in it and the Controller as the name suggests
-//  @RestController --> defines a class as a Component since it contains the @Controller annotation within it which has @Component within it thus also defining the class as a Controller;
+//  @Controller/@RestController, @Service and @Repository annotations are specializations of @Component annotation.
+//      Check here:--> https://stackoverflow.com/questions/6827752/whats-the-difference-between-component-repository-service-annotations-in
+//  Controllers however are not just any class, they are an app resource given they are the Presentational layer of the app container having the app's CRUD operations defined there in
+//  @Controller --> a stereotype annotation that clearly defines a class as both a Controller & a Component given it has the @Component annotation in it and the Controller as the name suggests
+//  @RestController --> also defines a class as both a Controller & a Component since it contains the @Controller annotation within it which has @Component within it thus also defining the class as a Controller;
 //                      and also has the @ResponseBody annotation within it, an annotation that allows the class to return Response Body objects in either JSON or XML formats
 //  @RequestMapping --> maps requests from the client to the method annotated and can have params, such as,
-//                      value - that assigns the endpoint url path and,
-//                      method - defines the CRUD operation request from the client
+//                      value - that assigns the endpoint URL path as a String and,
+//                      method - defines the CRUD operation request from the client as a RequestMethod call
 //                  To avoid being verbose; @RequestMapping has some other more specific annotations:
-//                  @GetMapping - for GET method and then you just define the endpoint path
-//                  @PostMapping - for POST method
-//                  @DeleteMapping - for DELETE method
+//                  @GetMapping - for GET methods and then you just define the endpoint path
+//                  @PostMapping - for POST methods
+//                  @DeleteMapping - for DELETE methods
+//                  @PutMapping - for UPDATE methods
 
-//  @RequestBody --> annotation that tells Spring Boot to get whwtever JSON Data it is getting and convert it into the declared Object
+//  @RequestBody --> annotation that tells Spring Boot to take whatever JSON Data it is getting and convert it into the declared Object;
+//               --> mainly used in methods that send back Data to the DB i.e in @PostMapping & @PutMapping annotated methods
 
 
 //    Entity
-//  @Entity --> is a persistence API interface that enables our Entity to interact with the DB
+//  @Entity --> is a persistence API interface that enables our defined Entity Object to interact with the DB
 //  @Id --> annotation that indicates the member field below is the primary key of current entity, inherited from javax.persistence.Id，
 //      --> placed on top of the property that is to be used as the Primary key
 //  @GeneratedValue --> annotation that configures the way of increment of the specified column(field).
-//                  --> contains a strategy as a parameter defining how the primary key should be generated
+//                  --> contains a strategy as a parameter defining how the primary key should be generated using the GenerationType call
 
 
 //    Service
-//  @Service --> stereotype annotation for the Service/Business layer
+//  @Service --> stereotype annotation for the Service/Business logic layer
 
 
 //    Repository
-//  @Repository --> stereotype annotation for the Persistence layer; the Data Access Object
+//  @Repository --> stereotype annotation for the Persistence layer; the Data Access Object(DAO)
 
 
 //  @Autowired --> annotation that tells Spring Boot to Autowire the defined Object and create it and attach it to the particular reference created
 //              hence allowing the use of Dependency Injection and Inversion Control of Spring's functionality feature
+//                  Check here:--> https://www.baeldung.com/spring-autowire
+//              --> when Autowiring a class that is Implementing an Interface that is being Implemented more than once by different classes;
+//              Spring will throw a NoUniqueBeanDefinitionException exception. This is avoided by narrowing the Implementation using a @Qualifier annotation
+//                  Check here:--> https://stackoverflow.com/questions/40830548/spring-autowired-and-qualifier#:~:text=The%20%40Qualifier%20annotation%20is%20used,constructor%20arguments%20or%20method%20parameters.
+//  @Qualifier --> annotation used to resolve the autowiring conflict, when there are multiple beans of the same type
 
 
 //      https://stackoverflow.com/questions/13715811/requestparam-vs-pathvariable
-//  @PathVariable() --> annotation used to identify the patter that is used in the URI for the incoming request
+//  @PathVariable() --> annotation used to identify the pattern that is used in the URI for a request being actioned per a certain criteria
 //                 --> annotation used to obtain some placeholder from the URI
 //  @RequestParam() --> annotation used for accessing the query parameter values from the request
 //                 --> annotation used to obtain a parameter from the URI
@@ -63,22 +71,27 @@ public class HelloWorld {
 
 
 //    Web Development architectural structure:
-//    1. The Front End technologies will consume the APIs to communicate with the DB
-//    2. Then the Back End responsible for providing the APIs
+//    1. Then the Back End team is responsible for creating the APIs
+//    2. The Front End technologies will consume the APIs to communicate with the DB
+
 //         Spring Boot's REST API architecture:
 //             1. Controller Layer      ===> this is the Presentational layer
 //                                      ===> it basically handles the client requests and gives back responses through the CRUD; POST, GET, UPDATE, DELETE operations
-//                                      ===> this is a Class
+//                                      ===> it passes its methods to the Service layer through the Autowired Service Interface
+//                                      ===> this is a Java Class
 //             2. Service Layer     ===> this is the Business logic layer
 //                                  ===> it contains the logic for our APIs
-//                                  ===> it basically implements the Repository/DAO layer to provide logic for how and what to do with the Data contained in the DataBase
-//                                  ===> this is a Class
-//             3. Data Access Object(DAO)       ===> this is the Repository layer or the Repository layer or the Persistence layer
-//                                              ===> it is the Data Access Layer of our API since it is the layer that interacts with the DataBase
-//                                              ===> this interface and allows us to work with the Spring Data JPA to work on our Entity
+//                                  ===> the layer as general will have an Interface and an Implementation class for the Interface
+//                                  ===> the Interface layer takes in the client request methods created in the Controller layer
+//                                  ===> the Implementation layer then Implements the methods defined in its Interface with added business logic and then calling the Autowired DAO layer for actioning in the DB
+//                                  ===> this comprises an Interface and a Java Impl Class that implements the Interface
+//             3. Repository layer       ===> this is the Data Access Object(DAO) layer or the Persistence layer
+//                                       ===> it is the Data Access Layer of our API since it is the layer that interacts with the DataBase
+//                                       ===> it extends JpaRepository interface that takes in the Entity/Bean Object defining our DB and the Entity property Type defined as our Primary Key for the DB
+//                                       ===> this is an Interface and thus allows us to work with the Spring Data JPA through extending it
 //             4. DataBase      ===> where our Data is stored
 //                              ===> Spring Boot offers H2 in-memory DB which can later be switched to a real DB such as PostgreSQL or mySQL or MSSQL
-//                              ===> Spring Boot provides us with a browser-based console that helps us interact with our H2 in-memory DB
+//                              ===> Spring Boot h2 dependency provides us with a browser-based console that helps us interact with our H2 in-memory DB
 
 //  Entity
 //    A default Model can be equated to an Entity in Spring Boot
@@ -90,37 +103,37 @@ public class HelloWorld {
 
 //  Controller
 //    Create the EntityController Class where the RESTful APIs will be created';
-//    and annotate it with the @RestController annotation
-//    After creating the Controller, you call the Service Layer which will pass the logic to the DAO
+//      and annotate it with the @RestController annotation
+//    After creating the Controller, you call the Service Layer Interface which will pass the methods created to the Service Implementation class for Implementation
 
 //  Service
-//    The convention is Creating an Interface for our service and then create the Class Implementing the service;
-//    given a Service can have several Implementations - thus create an Interface then create the class implementing that Service Interface
+//    The convention is Creating an Interface for our service and then create the Class Implementing the service interface;
+//      given an Interface can have several Implementations in different classes -  create an Interface then create the class implementing that Service Interface
 //    Thus the Interface in the Service package and then create the Impl package that'll contain the Impl Classes for the Interfaces.
-//    This Interface doesn't have to have the @Interface annotation, but the ServiceImpl Class must have the @Service annotation
+//    This Interface shouldn't have the @Repository annotation, but the ServiceImpl Class must have the @Service annotation
 
 
 //    Repository
 //  The Repository Class must have the @Repository annotation.
 //  JpaRepository will be extended for this project which will take in 2 properties;
 //      1st one will be the Entity defining our DB Properties, then the
-//      2nd one will be the Type for the Entity Property being used as the Primary Key
-//  JpaRepository gives us a lot of methods that we can use directly for interacting our Entity with the DB
+//      2nd one will be the Type for the Entity Property being used as the Primary Key, having the @Id annotation
+//  JpaRepository gives us several commonly used methods that we can use directly for interacting our Entity with the DB eg. findById(), findAll()
 
 
 
 //    Design Flow for the saveDepartment POST API call
 //    ***   Controller Layer Flow
-//  Create the saveDepartment method with the @RequestBody annotation referencing to the Created Entity;
+//  Create the saveDepartment method with the @RequestBody annotation referencing to the Entity where the Data Object is to be mapped to in the DB;
 //  @PostMapping() annotation is used with the path parameter defined inside the parenthesis
-//  In the same Controller, Inject the Service Layer Impl class that communicates with the DAO;
-//  Then return the newly created ServiceImpl Class calling the saveDepartment method which takes in the Class-level created Entity.
+//  In the same Controller, Inject the Service Layer Interface that will pass the created methods to the Service Impl class that passes the same to the DAO;
+//  Then return the Autowired Service Interface calling the saveDepartment method which takes in the Class-level created Entity Object.
 //    ***   Service Layer Flow
 //  Create saveDepartment method in the Service Layer Interface which will be implemented in the ServiceImpl Class;
 //  In the ServiceImpl Class, create the same saveDepartment method that takes in the Department Entity,
-//  and it will be an @Override annotated method;
+//      and it will be an @Override annotated method;
 //  Then Autowire the Repository Class which will connect us to the DB and allow us to use JPA methods;
-//  Then return the newly created/referenced Repository class calling JPA save methods that takes in the class-referenced department Entity.
+//  Then return the newly created/referenced Repository class calling JPA save method that takes in the class-referenced department Entity.
 
 
 
@@ -129,8 +142,8 @@ public class HelloWorld {
 //  Create the fetchDepartment method which fetches the Data available in the Entity as a List<> that takes in the Department Entity;
 //  @GetMapping() annotation is used with the path parameter defined inside the parenthesis
 //  fetchDepartment() will not take in any input since we are fetching Data from the Entity
-//  The Service Layer Impl class that communicates with the DAO is already injected;
-//  Then return the newly created ServiceImpl Class calling the fetchDepartment method which does not take in any input.
+//  The Service Layer Interface to be Implemented is already injected;
+//  Then return the Autowired Service Interface calling the fetchDepartment method which does not take in any input.
 //    ***   Service Layer Flow
 //  Create fetchDepartment method in the Service Layer Interface which will be implemented in the ServiceImpl Class;
 //  In the ServiceImpl Class, create the same fetchDepartment method which fetches the Data available in the Entity as a List<> that takes in the Department Entity,
