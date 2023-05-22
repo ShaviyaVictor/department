c5a3115c5e53798d21c83325fd64bc9fab08f131
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-22 16:20:51 -0500

    Create the Global REST Controller Class that will handle all Exceptions being thrown from tyhe Controller layer

---
+++ src/main/java/com/shavic/department/exception/handler/GlobalRestResponseEntityExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.shavic.department.exception.handler;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Class responsible for handling all Exceptions
+ *
+ */
+
+public class GlobalRestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+}
