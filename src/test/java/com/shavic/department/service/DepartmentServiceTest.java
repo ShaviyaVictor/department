ab26c6556683436d9851048d25f3e0967cc537bc
ShaviyaVictor <shaviyavictor@gmail.com>
2023-05-26 20:09:18 -0500

    Create the Mock Object that is expected using the Builder annotation earlier added

--- src/test/java/com/shavic/department/service/DepartmentServiceTest.java
+++ src/test/java/com/shavic/department/service/DepartmentServiceTest.java
@@ -2,10 +2,13 @@ package com.shavic.department.service;
 
 import com.shavic.department.entity.Department;
 import com.shavic.department.exception.DepartmentNotFoundException;
+import com.shavic.department.repository.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
 
@@ -14,11 +17,29 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class DepartmentServiceTest {
 
+//    Inject the service layer interface for the method call
     @Autowired
     private DepartmentService departmentService;
 
+//    Inject the repository layer which should be mocked for data retrieval
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
     @BeforeEach
     void setUp() {
+
+//        Create the mock object which will be built before every method is run
+        Department department = Department.builder()
+                .departmentId(1L)
+                .departmentName("Group IT")
+                .departmentHead("Shaviya")
+                .departmentRole("Software Engineering")
+                .build();
+
+//        tell Mockito to be providing the provided dummy data whenever the DAO method is called
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("Group IT"))
+                .thenReturn(Optional.ofNullable(department));
+
     }
 
     @Test
@@ -28,8 +49,11 @@ class DepartmentServiceTest {
         String departmentName = "Group IT";
 //        calling the method being tested thru the autowired layer that contains the method being tested
         Optional<Department> nameFound = departmentService.fetchDepartmentByName(departmentName);
+
 //        validate the values against each other, the expected value against the value found in the object returned form the test method call
-        assertEquals(departmentName, nameFound.stream());
+        assertEquals(departmentName, nameFound.get());
+
+
 
     }
 
