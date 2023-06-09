613ecae559aaebd2c5b3ff96ff180cd3cb73c30e
ShaviyaVictor <shaviyavictor@gmail.com>
2023-06-08 19:38:13 -0500

    Create the test classes for the 2 methods to be tested from the Controller layer

---
+++ src/test/java/com/shavic/department/controller/DepartmentControllerTest.java
@@ -0,0 +1,22 @@
+package com.shavic.department.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveDepartment() {
+    }
+
+    @Test
+    void fetchDepartmentById() {
+    }
+
+}
