43861a8f15cf2f9c1b6f0daa5493159865f835d9
ShaviyaVictor <shaviyavictor@gmail.com>
2023-08-04 18:23:53 -0500

    Create the Map to hold the features of our endpoint

--- src/main/java/com/shavic/department/config/FeatureEndpoint.java
+++ src/main/java/com/shavic/department/config/FeatureEndpoint.java
@@ -3,7 +3,14 @@ package com.shavic.department.config;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
-@Endpoint
+@Endpoint(id = "features")
 public class FeatureEndpoint {
+
+    private final Map<String, Feature> featureMap = new ConcurrentHashMap<>();
+
+
 }
