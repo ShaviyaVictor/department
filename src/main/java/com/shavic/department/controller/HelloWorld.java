d957fa0b102104c306597777729f4fb57c213eea
ShaviyaVictor <shaviyavictor@gmail.com>
2023-08-03 20:42:30 -0500

    Add the management tree pathy for actuator web endpoints

--- src/main/java/com/shavic/department/controller/HelloWorld.java
+++ src/main/java/com/shavic/department/controller/HelloWorld.java
@@ -714,10 +714,9 @@ public class HelloWorld {
  Actuator - is for managing and monitoring our application perfomance;
  - health    - beans created     - info     - memnory utilization and more
  Just add the Spring Actuator dependency in the pom file
- Run the application and some 2 default endpoints will be added; - " Exposing 1 endpoint(s) beneath base path '/actuator' "
-    one for Health and the other for Application Info
- You can enable the others by adding a configuration in the configuration yaml file
- management > endpoints > web/jmx > exposure > includes:"*"
+ Run the application and some 2 default endpoints will be added; - " Exposing 1 endpoint(s) beneath base path '/actuator' " - one for Health
+ You can enable the others by adding a configuration in the configuration yaml file - & with web 12 more endpoints will be added
+ management > endpoints > web/jmx > exposure > include:"*"
 
  TODO - there's more you can do with the Actuator, find the time and go read the Docs and play around with it
 
